ed3ac1ec0de5c3dddb66594c41b93cdcf8a47047
Anthony Sotera <s.terrainc@gmail.com>
2026-02-14 18:41:39 -0500

    Created htm for header.php with logo and simple nav, also created simple footer

---
+++ assignments/CourseProject/includes/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <p> Hockey Team Tracker COMM2006 </p>
+    <p> Course Project - Phase 1</p>
+</footer>
---
+++ assignments/CourseProject/includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hockey Team Tracker</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="../css/syles.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <h1 class="site-title">
+            <img
+                src="../images/hockeyLogo.jpg"
+                alt="Generic hockey Logo with two sticks"
+                class="logo">
+        </h1>
+        <nav>
+            <a href="../index.php"> Home </a>
+            <a href="../teamList.php"> Team List </a>
+        </nav>
+    </header>
